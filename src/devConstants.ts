43fbefc8ea01e6a093d529116764351a1e0f4ad6
Ivan Diaz <ivandiazwm@gmail.com>
2022-10-05 17:55:06 -0300

    Add kovan to goerli contract migration

--- src/devConstants.ts
+++ src/devConstants.ts
@@ -2,12 +2,12 @@ import { TiltedFinanceEnvironment } from "./types"
 
 const devConstants: TiltedFinanceEnvironment = {
   tiltChain: {
-    chainId: 42,
-    chainSlug: 'kovan',
-    tiltDistributorAddress: '0xee80a0cffa49b0e6aa9504eb3e1a9d86e5fc44aa',
-    tiltTokenAddress: '0x031852B8dE042B3f40C4012F085De0C203F173CA',
-    tiltDistributorTimeLockAddress: '0xCfd672B265C81f7eC0898E6Fea5B64Ac8D46FCDb',
-    tiltDistributorTimeLockProposerAddress: '0x8ed19f62a263398d49b80689d1d31624e7ebeac0',
+    chainId: 5,
+    chainSlug: 'goerli',
+    tiltDistributorAddress: '0xda02F7454d84f8A8D246606fC1be95f4808ee0FF',
+    tiltTokenAddress: '0xA7e5Af8b7f447B3B68bEeCb9A9795Da158697b1b',
+    tiltDistributorTimeLockAddress: '0x26789f598C5BDb67ef05eF7859788D4f55612d95',
+    tiltDistributorTimeLockProposerAddress: '0x2245e690fa1dbcf3fc49182e19298545d513aa0f',
     developerVestingWalletAddress: '',
     treasuryContractAddress: '',
   },
@@ -26,8 +26,8 @@ const devConstants: TiltedFinanceEnvironment = {
     tiltBatchRegistryPublisherAddress: '0xb5128c483fa56d15613d7afd8310cd29e9486a17',
   },
   merkleTree: {
-    blocksPerDistributionUpdate: 50000, // 27 hours
-    blocksForNextProposalUpdate: 50000,
+    blocksPerDistributionUpdate: 5555, // 3 hours
+    blocksForNextProposalUpdate: 5555,
   },
   publicEndpoints: {
     merkleTreeAPI: 'https://merkletree-dev.tilted.finance',
